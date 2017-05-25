1d4a107171617a54176e9caf5efdc3c3d797d760
Scott <mymislife@gmail.com>
2017-05-25 21:14:06 +0800

    Setting up environment

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Scott on 2017/5/25.
+ */
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance(){
+        return new BeatBoxFragment();
+    }
+}
